cc2fe36910167d9937d15aa1acb8c4a8fb022af8
sahilsanwal95 <your-email@example.com>
2025-03-29 11:01:58 +0530

    Added loginUser function

--- src/api/reqresApi.js
+++ src/api/reqresApi.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-const API_URL = "https://reqres.in/api/users"; 
+const API_URL = "https://reqres.in/api"; 
+
+export const loginUser = async (email, password) => {
+  const response = await axios.post(`${API_URL}/login`, { email, password });
+  return response;
+};
 
 export const getUserById = async (id) => {
-  const response = await axios.get(`${API_URL}/${id}`);
+  const response = await axios.get(`${API_URL}/users/${id}`);
   return response.data;
 };
 
 export const updateUser = async (id, userData) => {
-  const response = await axios.put(`${API_URL}/${id}`, userData);
+  const response = await axios.put(`${API_URL}/users/${id}`, userData);
   return response.data;
 };
